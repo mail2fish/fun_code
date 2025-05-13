6fcda8dd1a0c76d59cb5c41b7dfd4880113eba78
JunYu <394580@qq.com>
2025-05-13 14:07:38 +0800

    chore(deploy): remove vendor directory from rsync script
    The vendor directory is no longer needed in the deployment process, so it has been removed from the rsync script to streamline the deployment

--- deploy/rsync.sh
+++ deploy/rsync.sh
@@ -3,4 +3,3 @@
 git push huo main
 rsync -avz --delete ./web/react-router-www/build/client huo:/root/src/fun_code/web/react-router-www/build
 rsync -avz --delete ./web/scratch/dist/ huo:/root/src/fun_code/web/scratch/dist
-rsync -avz --delete ./vendor huo:/root/src/fun_code/
