eec06b48695137f45015acc3122867ace0871c16
JunYu <394580@qq.com>
2025-06-24 20:06:46 +0800

    refactor(routes): update route paths for better organization

--- web/react-router-www/app/routes.ts
+++ web/react-router-www/app/routes.ts
@@ -5,11 +5,11 @@ export default [
 
     // user
     route("/www/user/dashboard", "routes/user/dashboard.tsx"),
-    route("/www/scratch_projects", "routes/scratch_projects.tsx"),
-    route("/www/scratch_project_histories", "routes/scratch_project_histories.tsx"),
-    route("/www/list_files", "routes/list_files.tsx"),
-    route("/www/user_share", "routes/user_share.tsx"),
-    route("/www/all_share", "routes/all_share.tsx"),
+    route("/www/scratch/projects", "routes/scratch_projects.tsx"),
+    route("/www/scratch/project/:projectId/histories", "routes/scratch_project_histories.tsx"),
+    route("/www/files/list", "routes/list_files.tsx"),
+    route("/www/shares/user", "routes/user_share.tsx"),
+    route("/www/shares/all", "routes/all_share.tsx"),
     route("/www/share/:shareId", "routes/share.tsx"),
 
     // admin
@@ -17,7 +17,7 @@ export default [
     route("/www/admin/list_users", "routes/admin/list_users.tsx"),
     route("/www/admin/edit_user/:userId", "routes/admin/edit_user.tsx"),
     route("/www/admin/create_user", "routes/admin/create_user.tsx"),
-    route("/www/admin/upload_files", "routes/admin/upload_files.tsx"),
+    route("/www/admin/files/upload", "routes/admin/upload_files.tsx"),
     route("/www/admin/create_class", "routes/admin/create_class.tsx"),
     route("/www/admin/list_classes", "routes/admin/list_classes.tsx"),
     route("/www/admin/edit_class/:classId", "routes/admin/edit_class.tsx"),
