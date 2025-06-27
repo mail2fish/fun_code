818c29f9077d6495effe3eca2d9456665f9f4027
JunYu <394580@qq.com>
2025-06-27 14:11:26 +0800

    refactor(model): update ClassCourse gorm tags to use unique composite index
    Replace individual indexes with a unique composite index on ClassID and CourseID to enforce uniqueness of class-course pairs

--- internal/model/class_course.go
+++ internal/model/class_course.go
@@ -8,14 +8,14 @@ import (
 
 // ClassCourse 班级与课程的关联表
 type ClassCourse struct {
-	ClassID     uint   `json:"class_id" gorm:"not null;index"`    // 班级ID
-	CourseID    uint   `json:"course_id" gorm:"not null;index"`   // 课程ID
-	StartDate   int64  `json:"start_date"`                        // 开始日期 Unix 时间戳
-	EndDate     int64  `json:"end_date"`                          // 结束日期 Unix 时间戳
-	IsPublished bool   `json:"is_published" gorm:"default:false"` // 是否发布
-	CreatedAt   int64  `json:"created_at"`                        // 创建时间 Unix 时间戳
-	UpdatedAt   int64  `json:"updated_at"`                        // 更新时间 Unix 时间戳
-	DeletedAt   *int64 `json:"deleted_at,omitempty" gorm:"index"` // 删除时间 Unix 时间戳
+	ClassID     uint   `json:"class_id" gorm:"not null;uniqueIndex:idx_class_course"`  // 班级ID
+	CourseID    uint   `json:"course_id" gorm:"not null;uniqueIndex:idx_class_course"` // 课程ID
+	StartDate   int64  `json:"start_date"`                                             // 开始日期 Unix 时间戳
+	EndDate     int64  `json:"end_date"`                                               // 结束日期 Unix 时间戳
+	IsPublished bool   `json:"is_published" gorm:"default:false"`                      // 是否发布
+	CreatedAt   int64  `json:"created_at"`                                             // 创建时间 Unix 时间戳
+	UpdatedAt   int64  `json:"updated_at"`                                             // 更新时间 Unix 时间戳
+	DeletedAt   *int64 `json:"deleted_at,omitempty" gorm:"index"`                      // 删除时间 Unix 时间戳
 }
 
 func (c *ClassCourse) TableName() string {
