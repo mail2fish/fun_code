41134a746c6b33a968f6741fdfcbef5cfb6f1f38
JunYu <394580@qq.com>
2025-09-05 19:54:28 +0800

    feat: Update Excalidraw build script and static file synchronization
    - Modified Excalidraw's build command to ensure VITE_HOST_URL is set to '' during build.
    - Added Excalidraw static file synchronization in the rsync script to ensure the latest built files are deployed correctly.

--- deploy/rsync.sh
+++ deploy/rsync.sh
@@ -3,3 +3,4 @@
 git push huo main
 rsync -avz --delete ./web/react-router-www/build/client huo:/root/src/fun_code/web/react-router-www/build
 rsync -avz --delete ./web/scratch/dist/ huo:/root/src/fun_code/web/scratch/dist
+rsync -avz --delete ./web/excalidraw/dist/ huo:/root/src/fun_code/web/excalidraw/dist
