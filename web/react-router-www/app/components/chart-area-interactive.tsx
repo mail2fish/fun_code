7bd8b7184cee7d152144e2c4d9bec61ac0470534
JunYu <394580@qq.com>
2025-09-17 14:46:14 +0800

    feat: Update dependency versions and optimize type imports
    - Downgrade React and React DOM versions to 18.3.1.
    - Update @types/react and @types/react-dom versions.
    - Optimize type imports in multiple components to use `type` keyword for better code readability.
    - Modify import paths in some components to ensure consistency.

--- web/react-router-www/app/components/chart-area-interactive.tsx
+++ web/react-router-www/app/components/chart-area-interactive.tsx
@@ -10,8 +10,8 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card"
+import { type ChartConfig } from "~/components/ui/chart"
 import {
-  ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
